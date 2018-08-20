5dbc0059490307a663255b470578546c9639215d
Brandolini <revitoop@gmail.com>
2018-08-20 08:56:37 -0500

    Removed dummy expense data from app.js

--- src/app.js
+++ src/app.js
@@ -20,18 +20,6 @@ import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({ description: "Water bill", amount: 3000, createdAt: 4000 })
-);
-store.dispatch(
-  addExpense({ description: "Gas Bill", amount: 4000, createdAt: 500000 })
-);
-store.dispatch(addExpense({ description: "Enema Bill", amount: 200000 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 // console.log(store.getState());
 const jsx = (
   <Provider store={store}>
